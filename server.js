70e61af2ef05010eb025f6e12a3cf488d61785f5
salahelden1818 <salaheldenabdelshafy@gmail.com>
2023-03-05 13:09:36 +0300

    Deleting Socket.io

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const dotenv = require("dotenv").config({
   path: "./config.env",
 });
 const mongoose = require("mongoose");
-const http = require("http");
-const socketIO = require("socket.io");
+// const http = require("http");
+// const socketIO = require("socket.io");
 const app = require("./app");
-const {
-  userIsAuthorized,
-  saveMessageToDatabase,
-  authMiddleware,
-} = require("./utils/realTimeMethods");
+// const {
+//   userIsAuthorized,
+//   saveMessageToDatabase,
+//   authMiddleware,
+// } = require("./utils/realTimeMethods");
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-const io = socketIO(server);
+// const io = socketIO(server);
 
 // MongoDB Connection
 mongoose.set("strictQuery", false);
@@ -25,34 +25,34 @@ mongoose
   .catch((e) => console.log("Something went wrong", e));
 
 // starting real time
-io.on("connection", (socket) => {
-  console.log(`New Connection ${socket.id}`);
+// io.on("connection", (socket) => {
+//   console.log(`New Connection ${socket.id}`);
 
-  // protect middleware only registered users can send message
-  io.use(authMiddleware);
+//   // protect middleware only registered users can send message
+//   io.use(authMiddleware);
 
-  // send message
-  socket.on("sendMessage", async (data) => {
-    if (await userIsAuthorized(data.projectId, socket.user.id)) {
-      await saveMessageToDatabase(data.projectId, socket.user.id, data.message);
+//   // send message
+//   socket.on("sendMessage", async (data) => {
+//     if (await userIsAuthorized(data.projectId, socket.user.id)) {
+//       await saveMessageToDatabase(data.projectId, socket.user.id, data.message);
 
-      socket.broadcast.to(`project-${data.projectId}`).emit("newMessage", data);
-    }
-  });
+//       socket.broadcast.to(`project-${data.projectId}`).emit("newMessage", data);
+//     }
+//   });
 
-  socket.on("joinRoom", (projectId) => {
-    if (userIsAuthorized(projectId, socket.user.id)) {
-      socket.join(`project-${projectId}`);
-    }
-  });
+//   socket.on("joinRoom", (projectId) => {
+//     if (userIsAuthorized(projectId, socket.user.id)) {
+//       socket.join(`project-${projectId}`);
+//     }
+//   });
 
-  socket.on("disconnect", (data) => {
-    socket.broadcast.to(`project-${data.projectId}`).emit("left-chat");
-  });
-});
+//   socket.on("disconnect", (data) => {
+//     socket.broadcast.to(`project-${data.projectId}`).emit("left-chat");
+//   });
+// });
 
 // Start Listening
 const port = process.env.PORT || 3000;
-server.listen(port, () => {
+app.listen(port, () => {
   console.log("Start Listening on port " + port);
 });
